b481b31f9d7c4d6b2c873ce468cc822a46af111c
Drew Schillinger <drew@doctorew.com>
2025-09-25 22:07:33 +0000

    Milestone 1 completed and building

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,9 @@
 import { PrismaClient } from '@prisma/client'
 
 declare global {
-  interface GlobalThis {
-    prisma: PrismaClient | undefined
-  }
+  var prisma: PrismaClient | undefined
 }
 
-export const prisma = globalThis.prisma ?? new PrismaClient()
+export const prisma = global.prisma ?? new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
